c5791294e135e02956a17742efebd924c150be55
person2314 <greencaleb@hotmail.com>
2016-06-30 12:44:34 +0930

    Add files via upload

--- Asteroids/main.js
+++ Asteroids/main.js
@@ -5,8 +5,6 @@ window.addEventListener('keydown', function(evt) { onKeyDown(evt); }, false);
 window.addEventListener('keyup', function(evt) { onKeyUp(evt); }, false);
 
 
-var player = document.createElement("img");
-player.src = "ship.png"
 
 var KEY_SPACE = 32;
 var KEY_LEFT = 37;
@@ -16,44 +14,53 @@ var KEY_DOWN = 40;
 var KEY_A = 65;
 var KEY_D = 68;
 
-var x = 10;
-var y = 10;
-var angle = 0;
-//var GodSpeed = 0;
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+
+var ASTEROID_SPEED = 0.8;
+var GOD_SPEED = 1;
+var GOD_TURN_SPEED = 0.04;
+var BULLET_SPEED = 1.5;
+
+var god =
+{
+	image: document.createElement("img"),
+	x: SCREEN_WIDTH/2,
+	y: SCREEN_HEIGHT/2,
+	width: 93,
+	height: 80,
+	directionX: 0,
+	directionY: 0,
+	angularDirection: 0,
+	rotation: 0
+};
+
+god.image.src = "ship.png";
+
 
 function onKeyDown(event)
 {
 	if(event.keyCode == KEY_UP)
 	{
-		var s = Math.sin(angle);
-		var c = Math.cos(angle);
-		var xSpeed = 0;
-		var ySpeed = 1;
-		x += (xSpeed * c) - (ySpeed * s);
-		y += (xSpeed * s) + (ySpeed * c);
+		god.directionY = 1;
 	}
 	
 	if(event.keyCode == KEY_DOWN)
 	{
-		var s = Math.sin(angle);
-		var c = Math.cos(angle);
-		var xSpeed = 0;
-		var ySpeed = -1;
-		x += (xSpeed * c) - (ySpeed * s);
-		y += (xSpeed * s) + (ySpeed * c);
+		god.directionY = -1;
 	}
 	
 	if(event.keyCode == KEY_LEFT)
 	{
-		angle -= 0.02;
+		god.angularDirection = -1;
 	}
 	
 	if(event.keyCode == KEY_RIGHT)
 	{
-		angle += 0.02;
+		god.angularDirection = 1;
 	}
 	
-	if(event.keyCode == KEY_A)
+	/*if(event.keyCode == KEY_A)
 	{
 		var s = Math.sin(angle);
 		var c = Math.cos(angle);
@@ -71,22 +78,51 @@ function onKeyDown(event)
 		var ySpeed = 0;
 		x += (xSpeed * c) - (ySpeed * s);
 		y += (xSpeed * s) + (ySpeed * c);
-	}
+	}*/
 }
 
 function onKeyUp(event)
 {
+	if(event.keyCode == KEY_UP)
+	{
+		god.directionY = 0;
+	}
+	if(event.keyCode == KEY_DOWN)
+	{
+		god.directionY = 0;
+	}
+	if(event.keyCode == KEY_LEFT)
+	{
+		god.angularDirection = 0;
+	}
+	if(event.keyCode == KEY_RIGHT)
+	{
+		god.angularDirection = 0;
+	}
 }
 
 function run()
 {
-	context.fillstyle = "#ccc";
+	context.fillStyle = "#000066";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
+	var s = Math.sin(god.rotation);
+	var c = Math.cos(god.rotation);
+	
+	var xDir = (god.directionX * c) - (god.directionY * s);
+	var yDir = (god.directionX * s) + (god.directionY * c);
+	var xVel = xDir * GOD_SPEED;
+	var yVel = yDir * GOD_SPEED;
+	
+	god.x += xVel;
+	god.y += yVel
+	
+	god.rotation += god.angularDirection * GOD_TURN_SPEED;
+	
 	context.save();
-		context.translate(x, y);
-		context.rotate(angle);
-		context.drawImage(player, -player.width/2, -player.height/2);
+		context.translate(god.x, god.y);
+		context.rotate(god.rotation);
+		context.drawImage(god, -god.width/2, -god.height/2);
 	context.restore();
 	
 }
